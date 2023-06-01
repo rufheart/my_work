779f319561531a10990aa4f9c17d7a9e0881f883
Rufheart <rufetnuriyev.nr@gmail.com>
2023-06-02 00:54:00 +0400

    edit-twenty-seven

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -102,7 +102,6 @@ function Enterance(){
     let [IsActive, setIsActive] = useState('chat')
     let [menu_or_arrow, setMenuArrow] = useState(false)
     let [send, setSend] = useState(false)      
-    let [hovered, setHovered] = useState(false);
     let [search_menu, setSearchMenu] = useState(false)
     let [search_select, setSearchSelect] = useState('chats')
     let [input_value, setInputValue] = useState()
@@ -139,8 +138,8 @@ function Enterance(){
             <div className="left">
                 <div className="header">
                     <button className="button">
-                        {sudo==false?<span class="material-symbols-outlined" style={menu_or_arrow==true?{"transform":"rotate(90deg)","transition":"transform 0.3s"}:{"transform":"rotate(0deg)","transition":"transform 0.1s"}} onClick={() => {inputRef.current.focus()}}> menu </span>:
-                        <span class="material-symbols-outlined" onClick={Reverse} style={menu_or_arrow==true?{"transform":"rotate(90deg)","transition":"transform 0.1s"}:{"transform":"rotate(0deg)","transition":"transform 0.3s"}}>arrow_downward</span>}
+                        {sudo==false?<span class="material-symbols-outlined" style={menu_or_arrow==true?{"transform":"rotate(90deg)","transition":"transform 0.4s"}:{"transform":"rotate(0deg)","transition":"transform 0.2s"}} onClick={() => {inputRef.current.focus()}}> menu </span>:
+                        <span class="material-symbols-outlined" onClick={Reverse} style={menu_or_arrow==true?{"transform":"rotate(90deg)","transition":"transform 0.2s"}:{"transform":"rotate(0deg)","transition":"transform 0.4s"}}>arrow_downward</span>}
                     </button>
                     <div>
                         <input type="text" placeholder='Search' onClick={Icon_Changer} ref={inputRef} onChange={e=>InputChange(e)}/>
@@ -178,41 +177,67 @@ function Enterance(){
                         {search_menu==true?
                             <div className='search-menu'>
                             <div className='search-menu-header'>
-                                <button id='search-menu-header-chats' className={IsActive=='chat'?'active':null} onClick={()=>setIsActive('chat')}><span>Chat</span><span></span></button>
-                                <button id='search-menu-header-media' className={IsActive=='media'?'active':null} onClick={()=>setIsActive('media')}><span>Media</span><span></span></button>
-                                <button id='search-menu-header-links' className={IsActive=='link'?'active':null} onClick={()=>setIsActive('link')}><span>Links</span><span></span></button>
-                                <button id='search-menu-header-files' className={IsActive=='file'?'active':null} onClick={()=>setIsActive('file')}><span>Files</span><span></span></button>
-                                <button id='search-menu-header-music' className={IsActive=='music'?'active':null} onClick={()=>setIsActive('music')}><span>Music</span><span></span></button>
-                                <button id='search-menu-header-voice' className={IsActive=='voice'?'active':null} onClick={()=>setIsActive('voice')}><span>Voices</span><span></span></button>
+                                <button id='search-menu-header-chats' className={IsActive=='chat'?'active':null} onClick={()=>{setIsActive('chat');setSearchSelect('chats')}}><span>Chat</span><span></span></button>
+                                <button id='search-menu-header-media' className={IsActive=='media'?'active':null} onClick={()=>{setIsActive('media');setSearchSelect('media')}}><span>Media</span><span></span></button>
+                                <button id='search-menu-header-links' className={IsActive=='link'?'active':null} onClick={()=>{setIsActive('link');setSearchSelect('links')}}><span>Links</span><span></span></button>
+                                <button id='search-menu-header-files' className={IsActive=='file'?'active':null} onClick={()=>{setIsActive('file');setSearchSelect('files')}}><span>Files</span><span></span></button>
+                                <button id='search-menu-header-music' className={IsActive=='music'?'active':null} onClick={()=>{setIsActive('music');setSearchSelect('music')}}><span>Music</span><span></span></button>
+                                <button id='search-menu-header-voice' className={IsActive=='voice'?'active':null} onClick={()=>{setIsActive('voice');setSearchSelect('voices')}}><span>Voices</span><span></span></button>
                             </div>
                             <div className='search-menu-body'>
                                 {search_select=='chats'?
-                                <div>
-                                    {data.map((value, index)=>{
-                                        if(value.username==input_value){
-                                            console.log('yehu')
-                                        }
-                                    return(           
-                                        <NavLink htmlFor='zz' to={'/'+value.username.toLowerCase()+' '+value.surname.toLowerCase()} onClick={()=>setSend(true)} key={index}>
-                                            <div className="message-div">
-                                                <div className="left">                                           
-                                                    <div className="image"></div>
+                                    <div>
+                                        {data.map((value, index)=>{
+                                            if(value.username==input_value){
+                                                console.log('yehu')
+                                            }
+                                        return(           
+                                            <NavLink htmlFor='zz' to={'/'+value.username.toLowerCase()+' '+value.surname.toLowerCase()} onClick={()=>setSend(true)} key={index}>
+                                                <div className="message-div">
+                                                    <div className="left">                                           
+                                                        <div className="image"></div>
+                                                    </div>
+                                                    <div className="center">
+                                                        <div className="username">{value.username} {value.surname}</div>
+                                                        <div className="last-message"><i className="fas fa-check"></i> Salam necesiz? ne vaxt geleceksiz? helloasasasas
+                                                        asasasasasasasasasa
+                                                        saasasasas</div>
+                                                    </div>
+                                                    <div className='right'>
+                                                        <div className="date">Sonday</div>
+                                                        <div className='messages'>12</div>
+                                                    </div>
                                                 </div>
-                                                <div className="center">
-                                                    <div className="username">{value.username} {value.surname}</div>
-                                                    <div className="last-message"><i className="fas fa-check"></i> Salam necesiz? ne vaxt geleceksiz? helloasasasas
-                                                    asasasasasasasasasa
-                                                    saasasasas</div>
-                                                </div>
-                                                <div className='right'>
-                                                    <div className="date">Sonday</div>
-                                                    <div className='messages'>12</div>
-                                                </div>
-                                            </div>
-                                        </NavLink>
-                                        )
-                                    })}
-                                </div>:null}
+                                            </NavLink>
+                                            )
+                                        })}
+                                    </div>:null
+                                }
+                                {search_select=='media'?
+                                    <div>
+                                        media
+                                    </div>:null
+                                }
+                                {search_select=='links'?
+                                    <div>
+                                        Links
+                                    </div>:null
+                                }
+                                {search_select=='files'?
+                                    <div>
+                                        Files
+                                    </div>:null
+                                }
+                                {search_select=='music'?
+                                    <div>
+                                        Music
+                                    </div>:null
+                                }
+                                {search_select=='voices'?
+                                    <div>
+                                        Voices
+                                    </div>:null
+                                }
                             </div>
                         </div>:null
                         }                        
