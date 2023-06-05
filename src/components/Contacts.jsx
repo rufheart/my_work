4b15f7532a8a013c6867f3895d762bda34095dd9
rufheart <rufet>
2023-06-05 17:26:46 +0400

    edit-twenty-nine

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -6,65 +6,73 @@ function Contacts(){
     let [button,setButton] = useState(false)
     let [menu_or_arrow, setMenuArrow] = useState(false)
     let [main_menu, setMainMenu] = useState(true)
+    let [page_active, setPageActive] = useState('messages')
     function Menu_to_Arrrow(){
         setButton(true)
     }
     function Arrow_to_Menu(){
         setButton(false)
     }
+    function Page_to_Search(){
+        setPageActive('search')
+    }
+    function Page_to_Main(){
+        setPageActive('messages')
+    }
     return(
         <div className='contacts-main'>
             <div className='contacts-main-left'>
                 <div className="contacts-main-left-header">
                     <div className="button">
-                        <button>{button==false?<span><span style={menu_or_arrow==true?{"transform":"rotate(90deg)","transition":"transform 3s"}:{"transform":"rotate(0deg)","transition":"transform 0.8s"}} class="material-symbols-outlined"> menu </span></span>:<span><span class="material-symbols-outlined" style={menu_or_arrow==true?{"transform":"rotate(90deg)","transition":"transform 10s"}:{"transform":"rotate(0deg)","transition":"transform 0.4s"}} onClick={()=>(setTimeout(Arrow_to_Menu,2000),setMenuArrow(false),setMainMenu(true))}> arrow_downward </span></span>}</button>
+                        <button>{button==false?<div><span style={menu_or_arrow==true?{"transform":"rotate(90deg)","transition":"transform 0.4s linear"}:{"transform":"rotate(0deg)","transition":"transform 0.8s"}} class="material-symbols-outlined"> menu </span></div>:<div><span class="material-symbols-outlined" style={menu_or_arrow==true?{"transform":"rotate(90deg)","transition":"transform 9s"}:{"transform":"rotate(0deg)","transition":"transform 0.8s"}} onClick={()=>(setTimeout(Arrow_to_Menu,150),setTimeout(Page_to_Main,200),setMenuArrow(false),setMainMenu(true))}> arrow_downward </span></div>}</button>
                     </div>
-                    <div><input type="text" placeholder='Search' onClick={()=>(setTimeout(Menu_to_Arrrow, 2000),setMenuArrow(true),setMainMenu(false))}/><button><span class="material-symbols-outlined"> search </span>   </button></div>
+                    <div><input type="text" placeholder='Search' onClick={()=>(setTimeout(Menu_to_Arrrow, 150),setTimeout(Page_to_Search,200),setMenuArrow(true),setMainMenu(false))}/><button><span class="material-symbols-outlined"> search </span>   </button></div>
                 </div>
                 <div className='contacts-main-left-body'>
-                    {main_menu==true?<div className='contacts-main-left-body-main-menu'>
-                        <div className='contacts-main-left-body-top'>
-                            <NavLink to={'/'+'soz'}>
-                                <div><span>Gözləmədə(1)</span><i class="fa fa-address-card-o"></i></div>
-                                <div></div>
-                                <div><img src={require('../woman.png')} alt="" /><span>Veronica Belluci</span></div>
-                                <div></div>
-                                <div><span>Mövcuddur (4) </span><i class="fa fa-address-card-o"></i></div>
-                            </NavLink>
-                            <NavLink to=''>
-                                <div><span>Gözləmədə(1)</span><i class="fa fa-address-card-o"></i></div>
-                                <div></div>
-                                <div><img src={require('../woman.png')} alt="" /><span>Veronica Belluci</span></div>
-                                <div></div>
-                                <div><span>Mövcuddur (4) </span><i class="fa fa-address-card-o"></i></div>
-                            </NavLink>
-                        </div>
-                        <div className='contacts-main-left-body-bottom'>
-                            <div>
-                                <span>Invite to Chatgram</span>
+                    <div style={page_active=='messages'?{left:"0%",transition:"left 0.5s"}:{left:"-100%",transition:"left 1s"}} className='bir'>
+                        {main_menu==true?<div className='contacts-main-left-body-main-menu'>
+                            <div className='contacts-main-left-body-top'>
+                                <NavLink to={'/'+'soz'}>
+                                    <div><span>Gözləmədə(1)</span><i class="fa fa-address-card-o"></i></div>
+                                    <div></div>
+                                    <div><img src={require('../woman.png')} alt="" /><span>Veronica Belluci</span></div>
+                                    <div></div>
+                                    <div><span>Mövcuddur (4) </span><i class="fa fa-address-card-o"></i></div>
+                                </NavLink>
+                                <NavLink to=''>
+                                    <div><span>Gözləmədə(1)</span><i class="fa fa-address-card-o"></i></div>
+                                    <div></div>
+                                    <div><img src={require('../woman.png')} alt="" /><span>Veronica Belluci</span></div>
+                                    <div></div>
+                                    <div><span>Mövcuddur (4) </span><i class="fa fa-address-card-o"></i></div>
+                                </NavLink>
                             </div>
-                            <div>
+                            <div className='contacts-main-left-body-bottom'>
                                 <div>
-                                    <div>
-                                        <img src={require('../avatar.jpg')} alt="" />
-                                        <span>Enzo Malacia</span>
-                                    </div>
-                                    <div><button>Invite</button></div>
+                                    <span>Invite to Chatgram</span>
                                 </div>
                                 <div>
                                     <div>
-                                        <img src={require('../avatar.jpg')} alt="" />
-                                        <span>Enzo Malacia</span>
+                                        <div>
+                                            <img src={require('../avatar.jpg')} alt="" />
+                                            <span>Enzo Malacia</span>
+                                        </div>
+                                        <div><button>Invite</button></div>
                                     </div>
-                                    <div><button>Invite</button></div>
+                                    <div>
+                                        <div>
+                                            <img src={require('../avatar.jpg')} alt="" />
+                                            <span>Enzo Malacia</span>
+                                        </div>
+                                        <div><button>Invite</button></div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>:
-                    <div className='contacts-main-left-body-search-menu'>
-                        search
+                        </div>:null}
                     </div>
-                    }
+                    <div style={page_active=='search'?{left:"0%",transition:"left 0.5s"}:{left:"-100%",transition:"left 1s"}} className='iki'>
+                        soz
+                    </div>
                 </div>
             </div>
             <div className='contacts-main-right'>
